44387a2f10030922f0606baadf1d2ec85a952e68
Hadar Dahan <hadardh@mta.ac.il>
2021-02-17 11:30:50 +0200

    Only Health

---
+++ server/src/servlets/boats/addNewBoatServlet.java
@@ -0,0 +1,27 @@
+package servlets.boats;
+
+import bms.engine.boatsManagement.boat.Boat;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+@WebServlet(name = "addNewBoatServlet", urlPatterns = {"/boats/addNew"})
+public class addNewBoatServlet extends HttpServlet {
+
+    private Gson gson = new Gson();
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doPost(req, resp);
+        BufferedReader reader = req.getReader();
+        String newBoatJsonString = reader.lines().collect(Collectors.joining());
+
+        Boat newBoat = gson.fromJson(newBoatJsonString, Boat.class);
+    }
+}
--- server/web/boats/addNew.js
+++ server/web/boats/addNew.js
@@ -2,9 +2,25 @@
 const privateCheckBoxEl = document.querySelector('#checkPrivate');
 privateCheckBoxEl.addEventListener('change', privateChecked);
 
+const boatFormEl = document.querySelector('#addNewBoatForm');
+const oneRowerRadioEl = document.querySelector('#solo');
+const eightRowerRadioEl = document.querySelector('#eight');
+const coxswainRadioEl = document.querySelector('#coxswain');
+const oneOarRadioEl = document.querySelector('#singleOar');
+const formErrorEl = document.querySelector('#errorSpan');
+boatFormEl.addEventListener('submit', validateForm);
+
+const ONE_ROWER_ONE_OAR = "A boat with only one rower cannot be with single oar";
+const ONE_ROWER_WITH_COXS = "A boat with only one rower cannot have a coxswain";
+const EIGHT_ROWERS_NO_COXS = "A boat with eight rowers cannot be coxless";
+const NO_ERROR = '';
+
+//
+//privateCheckBoxEl.addEventListener('change', )
+
 function privateChecked() {
     // Get the checkbox
-    let checkBox = document.getElementById("checkPrivate");
+    let checkBox = privateCheckBoxEl;
     // Get the output text
     let boatOwner = document.getElementById("boatOwnerform");
 
@@ -14,5 +30,45 @@ function privateChecked() {
     } else {
         boatOwner.style.display = "none";
     }
+}
 
+function validateForm(event) {
+
+    if (oneRowerRadioEl.checked && oneOarRadioEl.checked) {
+        //A boat with only one rower cannot be with single oar
+        event.preventDefault();
+        showError(ONE_ROWER_ONE_OAR);
+    }else if (oneRowerRadioEl.checked && coxswainRadioEl.checked) {
+        //A boat with only one rower cannot have a coxswain
+        event.preventDefault();
+        showError(ONE_ROWER_WITH_COXS);
+    }else if (eightRowerRadioEl.checked && !coxswainRadioEl.checked) {
+        //A boat with eight rowers cannot be coxless
+        event.preventDefault();
+        showError(EIGHT_ROWERS_NO_COXS);
+    }
+
+    showError(NO_ERROR);
+        }
+
+
+
+async function submitBoat (name, isPrivate, isOutOfOrder, boatSize, oneOar, width, coxswain, coastalboat)
+{
+    const newBoatType ={
+        rowersNum:boatSize,
+        singleOar:oneOar,
+        wide:width,
+        helmsman:coxswain,
+        coastal:coastalboat
+    }
+    const newBoat = {
+        boatName:name,
+        privateProperty:isPrivate,
+        type: newBoatType
+    }
+}
+
+function showError(errorMsg) {
+    formErrorEl.textContent = "Cannot Add Boat: "+ errorMsg;
 }
