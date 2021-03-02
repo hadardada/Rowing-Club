56053f0f4592407dff3ff0d38f5beccc61ceb201
sdadmin <snir.dobo@gmail.com>
2021-03-02 21:43:06 +0200

    alll3

--- server/web/member/addNew.js
+++ server/web/member/addNew.js
@@ -31,8 +31,8 @@ const NO_ERROR = '';
 
 
 function validateForm(event) {
-
-    if (!(memberEmail.value.includes('@'))) {
+    let emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailRegex.test(memberEmail.value)) {
         event.preventDefault();
         showError(EMAIL_FORMAT);
 
